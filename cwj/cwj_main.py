997740fd725e7e3a4d542f2523cf59b22a6280e1
wonjun choi <astc8332@naver.com>
2025-07-10 16:36:05 +0900

    multiple knapsack added

--- cwj/cwj_main.py
+++ cwj/cwj_main.py
@@ -1,12 +1,16 @@
 import random
 
-from cwj_util import bin_packing
+from cwj_util import bin_packing, multiple_knapsack
 
 def main():
     capa = 100
-    weights = [random.randint(10, 20) for _ in range(40)]
+    weights = [random.randint(10, 20) for _ in range(40)]  # 아이템 무게
     log = True
     obj = bin_packing(weights, capa, log)
-    
-if __name__ == "__main__":
+    print("###############################################")
+    solutions = multiple_knapsack(weights, capa, obj, log=log)
+    print(solutions)
+
+
+if __name__ == '__main__':
     main()
