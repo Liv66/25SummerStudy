1058aaf5e0e46b0302af927a2d3f47f3078f4a52
Liv66 <jh.kim@pusan.ac.kr>
2025-07-10 16:30:47 +0900

    add multiple knapsack

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import random
+
+from util import bin_packing, multiple_knapsack
+
+
+def main():
+    capa = 100
+    weights = [random.randint(10, 20) for _ in range(40)]  # 아이템 무게
+    log = True
+    obj = bin_packing(weights, capa, log)
+    print("###############################################")
+    solutions = multiple_knapsack(weights, capa, obj, log=log)
+    print(solutions)
+
+
+if __name__ == '__main__':
+    main()
