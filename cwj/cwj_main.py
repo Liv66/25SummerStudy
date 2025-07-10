1d0cad93a9a7c354e79dda6ebc5e88a7dfdcea4d
wonjun choi <astc8332@naver.com>
2025-07-10 16:31:59 +0900

    bin packing added

--- cwj/cwj_main.py
+++ cwj/cwj_main.py
@@ -1,6 +1,12 @@
-def cwj_run():
-    print ("hello")
-    
+import random
+
+from cwj_util import bin_packing
+
+def main():
+    capa = 100
+    weights = [random.randint(10, 20) for _ in range(40)]
+    log = True
+    obj = bin_packing(weights, capa, log)
     
 if __name__ == "__main__":
-    cwj_run()
+    main()
