a47cd4e0bde9c79bd402f6760dcfadddb2a34509
김정헌 <rlawjdgjs73@gmail.com>
2025-07-17 04:31:09 +0900

    Implements VRPB solver
    Adds a solver for the Vehicle Routing Problem with Backhauls (VRPB).
    
    The solver uses a construction heuristic with both parallel and sequential insertion strategies.
    It generates instances and evaluates the solution.

--- util.py
+++ util.py
@@ -141,14 +141,20 @@ def multiple_knapsack(weights, capa, K, count=20, timelimit=10, log=False):
     else:
         print("ERROR : find solutions")
 
+
 """
     노드들의 좌표가 들어오면 거리행렬 반환
     dist_matrix[i][j] : i에서 j까지 거리
 """
+
+
 def get_distance(nodes_coord):
     N = len(nodes_coord)
-    return [[int(sqrt((nodes_coord[i][0] - nodes_coord[j][0]) ** 2 + (nodes_coord[i][1] - nodes_coord[j][1]) ** 2)) for i in range(N)] for j in
-            range(N)]
+    return [
+        [int(sqrt((nodes_coord[i][0] - nodes_coord[j][0]) ** 2 + (nodes_coord[i][1] - nodes_coord[j][1]) ** 2)) for i in
+         range(N)] for j in
+        range(N)]
+
 
 """
     depot : 출발 노드
@@ -156,6 +162,8 @@ def get_distance(nodes_coord):
     dist_matrix : 거리 행렬
     nodes_coord : 노드들 좌표
 """
+
+
 def two_opt(depot, nodes, dist_matrix, nodes_coord, show=False):
     improved = True
     result_route = [i for i in nodes] + [depot]
@@ -166,8 +174,10 @@ def two_opt(depot, nodes, dist_matrix, nodes_coord, show=False):
         for i in range(1, len(nodes) - 1):
             for j in range(i + 1, len(nodes)):
                 # before : 기존 경로 / after : 변경된 부분
-                before = (dist_matrix[result_route[i - 1]][result_route[i]] + dist_matrix[result_route[j]][result_route[j + 1]])
-                after = (dist_matrix[result_route[i - 1]][result_route[j]] + dist_matrix[result_route[i]][result_route[j + 1]])
+                before = (dist_matrix[result_route[i - 1]][result_route[i]] + dist_matrix[result_route[j]][
+                    result_route[j + 1]])
+                after = (dist_matrix[result_route[i - 1]][result_route[j]] + dist_matrix[result_route[i]][
+                    result_route[j + 1]])
                 if after - before < 0:
                     result_cost += after - before
                     result_route = result_route[:i] + list(reversed(result_route[i:j + 1])) + result_route[j + 1:]
@@ -191,6 +201,7 @@ def plot(nodes_coord, route, title=''):
     plt.title(title)
     plt.show()
 
+
 def plot_cvrp(nodes_coord, best_result_route, title=''):
     for route in best_result_route:
         points_x = [nodes_coord[x][0] for x in route]
@@ -200,3 +211,102 @@ def plot_cvrp(nodes_coord, best_result_route, title=''):
                  label='Line')
     plt.title(title)
     plt.show()
+
+
+def plot_vrpb(problem_info, best_result_route=[], title=''):
+    nodes_coord = problem_info['node_coords']
+    nodes_type = problem_info['node_types']
+    for i in range(len(nodes_type)):
+        if nodes_type[i] == 0:
+            plt.scatter(nodes_coord[i][0], nodes_coord[i][1], s=150, marker='s', color='black')
+        elif nodes_type[i] == 1:
+            plt.scatter(nodes_coord[i][0], nodes_coord[i][1], s=35, color='blue')
+        else:
+            plt.scatter(nodes_coord[i][0], nodes_coord[i][1], s=35, marker='^', color='red')
+
+    for route in best_result_route:
+        points_x = [nodes_coord[x][0] for x in route]
+        points_y = [nodes_coord[x][1] for x in route]
+        # plt.scatter(points_x, points_y)
+        plt.plot([points_x[i] for i in range(len(route))], [points_y[i] for i in range(len(route))], linestyle='-',
+                 label='Line', )
+    plt.title(title)
+    plt.xticks([])  # x축 눈금 없애기
+    plt.yticks([])  # y축 눈금 없애기
+    plt.show()
+
+
+def check_feasible(problem_info, sol, elapsed, timelimit):
+    K = problem_info['K']
+    node_type = problem_info['node_types']
+    node_demand = problem_info['node_demands']
+    capa = problem_info['capa']
+    dist_mat = problem_info['dist_mat']
+
+    if elapsed > timelimit + 1:
+        print("Time Out")
+        return 0
+
+    if len(sol) > K:
+        print(f"vehicle 수는 {K}대까지 사용 가능합니다. 현재 : {len(sol)}")
+        return 0
+
+    total_cost = 0
+    visit = [0] * len(node_type)
+    visit[0] = 1
+
+    for idx, route in enumerate(sol):
+        if route[0] != 0:
+            print(f"depot에서 출발해야 합니다. {idx}번째 차량 경로 {route}")
+            return 0
+
+        if route[-1] != 0:
+            print(f"depot으로 도착해야 합니다. {idx}번째 차량 경로 {route}")
+            return 0
+        if node_type[route[1]] == 2:
+            print(f"차량은 backhaul만 갈 수 없습니다. {idx}번째 차량 경로 {route}")
+            return 0
+        cost = 0
+        load = 0
+
+        pre = 0
+        flag = False
+        route_type = [0] * len(route)
+
+        for i in range(1, len(route) - 1):
+            nxt = route[i]
+            if visit[nxt]:
+                print(f"{nxt} 노드 2번 이상 방문")
+                return 0
+            visit[nxt] = 1
+            cost += dist_mat[pre][nxt]
+            load += node_demand[nxt]
+            route_type[i] = node_type[nxt]
+            if nxt == 0:
+                print(f"{idx}번째 차량 depot을 3번 이상 방문 {route}")
+                return 0
+
+            if node_type[pre] == 1 and node_type[nxt] == 2:
+                if flag:
+                    print(f"{idx}번째 차량 line -> backhaul -> line 경로 존재")
+                    print(node_type)
+                    return 0
+                flag = True
+                load = 0
+
+            if load > capa:
+                if flag:
+                    print(f"{idx}번째 차량의 line 용량 초과 {load}, {capa}")
+                else:
+                    print(f"{idx}번째 차량의 back 용량 초과 {load}, {capa}")
+                return 0
+            pre = nxt
+        cost += dist_mat[pre][0]
+        total_cost += cost
+
+    if sum(visit) < len(node_type):
+        yet_visit = [i for i in range(len(node_type)) if not visit[i]]
+        print(f"다음 노드들을 방문하지 않았습니다. {yet_visit}")
+        return 0
+
+    return total_cost
